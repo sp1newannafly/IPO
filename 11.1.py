8f2a76c08c84438b0914e0389a3b15818eef53c5
Maks <maks.prokopenkoo@yandex.com>
2021-12-22 13:22:25 +0300

    Update 11.1.py

--- 11.1.py
+++ 11.1.py
@@ -1,5 +1,16 @@
-r = int(input('Введите радиус - '))
-v = 3/4 * 3.14 * r ** 3
-s = 4*3.14*r*2 
-print(v,'Объем шара')
-print(s,'Площадь боковой поверхности шара')
+class Shar():
+    def sum(self):
+        v = int(input("Введите объем шара: "))
+        pi=3.14
+        r=int(input("Введите радиус шара: "))
+        rezl=(4/3)*pi*(r*r*r)
+        print("Объем шара: ", rezl)
+    def kub(self):
+        p=3.14
+        rr=int(input("Ввдеите радиус шара: "))
+        s=4*p*(rr*rr)
+        print("Площадь боковой поверхности шара: ", s)
+rezl=Shar()
+rezl.sum()
+s=Shar()
+s.kub()
